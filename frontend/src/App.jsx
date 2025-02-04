69829b74e996dda8a08b125d8619482a4e1d29e0
Nik Hixson <nikilobe@gmail.com>
2025-02-03 22:31:08 -0800

    Refactor App component to use PhotoList for rendering photos

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import PhotoListItem from './components/PhotoListItem';
+import PhotoList from './components/PhotoList';
 import './App.scss';
 
 const App = () => {
-  const photos = Array.from({ length: 3 }, (_, index) => ({
-    id: String(index + 1),
-    location: {
-      city: ["Montreal", "Toronto", "Ottawa"][index],
-      country: "Canada",
-    },
-    imageSource: `${process.env.PUBLIC_URL}/Image-${index + 1}-Regular.jpeg`,
-    username: "Joe Example",
-    profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-  }));
-
   return (
     <div className="App">
-      {photos.map(photo => (
-        <PhotoListItem key={photo.id} photo={photo} />
-      ))}
+      <PhotoList />
     </div>
   );
 };
