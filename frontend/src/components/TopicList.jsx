0c9f84cc02fa8d8615361bd8fce77124e1fe5d48
Nik Hixson <nikilobe@gmail.com>
2025-02-13 08:55:09 -0800

    Enhance TopicList and TopicListItem components to support topic selection; pass onTopicSelect prop and handle click events

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
-const TopicList = ({ topics }) => {
+const TopicList = ({ topics, onTopicSelect }) => {
   return (
     <div className="top-nav-bar__topic-list">
       {topics.map((topic) => (
         <TopicListItem
           key={topic.id}
+          id={topic.id}
           title={topic.title}
+          onTopicSelect={onTopicSelect}
         />
       ))}
     </div>
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-
 import "../styles/TopicListItem.scss";
 
-const TopicListItem = ({ title }) => {
+const TopicListItem = ({ id, title, onTopicSelect }) => {
+  const handleClick = () => {
+    if (onTopicSelect) onTopicSelect(id);
+  };
+
   return (
-    <div className="topic-list__item">
+    <div className="topic-list__item" onClick={handleClick}>
       <span>{title}</span>
     </div>
   );
